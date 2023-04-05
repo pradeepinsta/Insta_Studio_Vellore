799584999250378b4b5c85759f35c18a4ff99a95
prade <pradeepravi24@gmail.com>
2023-04-05 16:30:41 +0530

    Adding up signin

--- app/src/main/java/com/example/instastudiovellore/Signup.java
+++ app/src/main/java/com/example/instastudiovellore/Signup.java
@@ -1,14 +1,26 @@
 package com.example.instastudiovellore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
+
 public class Signup extends AppCompatActivity {
 
-    @Override
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //No Dark mode for whole App
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
         setContentView(R.layout.activity_signup);
+
+        //No dark mode for this activity
+        getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
+
+
 }

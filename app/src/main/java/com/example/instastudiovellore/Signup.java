cafcf9f8ce1d0bafffd2fed6c848b59b90398bda
prade <pradeepravi24@gmail.com>
2023-04-06 00:05:49 +0530

    Designed signin

--- app/src/main/java/com/example/instastudiovellore/Signup.java
+++ app/src/main/java/com/example/instastudiovellore/Signup.java
@@ -5,21 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class Signup extends AppCompatActivity {
+
+    ImageView logo;
+    Button phoneSignin;
+    Button googleSignin;
+    TextView textviewOr,textviewContactUs,textviewQueries;
 
         @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //No Dark mode for whole App
-//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-
         setContentView(R.layout.activity_signup);
 
-        //No dark mode for this activity
-        getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        logo = findViewById(R.id.imageViewLogo);
+        textviewOr = findViewById(R.id.textViewOr);
+        textviewContactUs = findViewById(R.id.textViewContactus);
+        textviewQueries = findViewById(R.id.textViewQueries);
+        phoneSignin = findViewById(R.id.button_phone);
+        googleSignin = findViewById(R.id.button_google);
+
+
     }
 
 

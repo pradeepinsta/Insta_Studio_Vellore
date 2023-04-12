a5aa99a41a9521da8f3c094b2a7b0ee9feb19b51
pradeepinsta <pradeepravi24@gmail.com>
2023-04-12 23:03:23 +0530

    Google SignIn Done

--- app/src/main/java/com/example/instastudiovellore/HomeActivity.java
+++ app/src/main/java/com/example/instastudiovellore/HomeActivity.java
@@ -2,13 +2,32 @@ package com.example.instastudiovellore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button signout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        signout = findViewById(R.id.buttonSignout);
+
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(HomeActivity.this,Signup.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/instastudiovellore/Signup.java
+++ app/src/main/java/com/example/instastudiovellore/Signup.java
@@ -1,5 +1,9 @@
 package com.example.instastudiovellore;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
@@ -10,6 +14,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class Signup extends AppCompatActivity {
@@ -18,12 +36,18 @@ public class Signup extends AppCompatActivity {
     Button phoneSignin;
     Button googleSignin;
     TextView textviewOr,textviewContactUs,textviewQueries;
+    GoogleSignInClient googleSignInClient;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+
+    ActivityResultLauncher<Intent> activityResultLauncher;
 
         @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_signup);
+
+        registerActivityForGoogleSignIn();
 
         logo = findViewById(R.id.imageViewLogo);
         textviewOr = findViewById(R.id.textViewOr);
@@ -53,8 +77,99 @@ public class Signup extends AppCompatActivity {
             }
         });
 
+        googleSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                signinGoogle();
+
+            }
+        });
+
 
     }
 
+    public void signinGoogle()
+    {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("575102053086-im1q0fdhov97s249t39tigvsmpet3m5g.apps.googleusercontent.com")
+                .requestEmail().build();
 
+        googleSignInClient = GoogleSignIn.getClient(this,gso);
+        signin();
+    }
+
+    public void signin()
+    {
+        Intent signinIntent = googleSignInClient.getSignInIntent();
+        activityResultLauncher.launch(signinIntent);
+    }
+
+    public void  registerActivityForGoogleSignIn()
+    {
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        int resultCode = result.getResultCode();
+                        Intent data = result.getData();
+
+                        if (resultCode == RESULT_OK && data != null)
+                        {
+                            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                            firebaseSignInWithGoogle(task);
+                        }
+
+                    }
+                });
+    }
+
+    private void firebaseSignInWithGoogle(Task<GoogleSignInAccount> task)
+    {
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            Toast.makeText(this, "Successful", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(Signup.this,HomeActivity.class);
+            startActivity(i);
+            finish();
+            firebaseGoogleAccount(account);
+        } catch (ApiException e) {
+           e.printStackTrace();
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void firebaseGoogleAccount(GoogleSignInAccount account)
+    {
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+        auth.signInWithCredential(authCredential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful())
+                        {
+
+                        }
+                        else
+                        {
+
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = auth.getCurrentUser();
+        if (user != null)
+        {
+            Intent i = new Intent(Signup.this,HomeActivity.class);
+            startActivity(i);
+            finish();
+        }
+    }
 }
